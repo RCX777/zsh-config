0b2366d8b8c70203df461a83b75f1c49f3af0477
Robert-Ioan Constantinescu <robertioan.constantinescu@keyight.com>
2025-03-20 19:40:16 +0000

    make aliases safe
    Signed-off-by: Robert-Ioan Constantinescu <robertioan.constantinescu@keyight.com>

--- aliases.zsh
+++ aliases.zsh
@@ -1,14 +1,28 @@
 alias cl="clear"
 
-alias vi="nvim"
-alias vim="nvim"
+if command -v "nvim" > /dev/null; then
+    alias vi="nvim"
+    alias vim="nvim"
+fi
 
-alias grep="rg"
+if command -v "rg" > /dev/null; then
+    alias grep="rg"
+fi
 
-alias ls="lsd"
-alias ll="lsd -l"
-alias la="lsd --almost-all"
-alias l="lsd -l --almost-all"
+if command -v "lsd" > /dev/null; then
+    alias ls="lsd"
+    alias ll="lsd -l"
+    alias la="lsd --almost-all"
+    alias l="lsd -l --almost-all"
+else
+    alias ll="ls -lh"
+    alias la="ls -a"
+    alias l="lsd -alh"
+fi
+
+if command -v "zoxide" > /dev/null; then
+    alias cd="z"
+fi
 
 alias tmux="SHELL=$(which zsh) tmux"
 
